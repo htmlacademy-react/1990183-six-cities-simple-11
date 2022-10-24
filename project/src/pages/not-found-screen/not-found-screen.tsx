fb45bf84332389baad21d08fcf176d1481780250
Christina Yanovich <christik.lucid@gmail.com>
2022-10-24 15:37:37 +0300

    Обновляет разметку компонента NotFoundScreen

--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,15 +1,30 @@
 import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
+
+import Header from '../../components/header/header';
 
 function NotFoundScreen() {
   return (
-    <div className="page page--gray page--not-found">
-      <main className="page__main page__main--not-found">
-        <div className="container">
-          <section className="not-found">
-            <h1 className="not-found__title">404</h1>
-            <p>Page Not Found</p>
-            <Link to="/">Home Page</Link>
+    <div className="page page--gray page--login">
+      <Header hasNavigation={false} />
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+
+          <section className="login">
+            <h1 className="login__title">Error 404</h1>
+            <p>Page not found</p>
+            <Link to={AppRoute.Root}>Home page</Link>
           </section>
+
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#todo">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
+
         </div>
       </main>
     </div>
