0a466ca5b88580c091e4fe35442fdae9c0505fb1
Christik <christik.lucid@gmail.com>
2022-10-29 11:07:33 +0300

    Фиксит значения инпутов в RatingForm

--- project/src/components/rating-form/rating-form.tsx
+++ project/src/components/rating-form/rating-form.tsx
@@ -9,26 +9,30 @@ const ratingStars: string[] = [
 function RatingForm() {
   return (
     <div className="reviews__rating-form form__rating">
-      {ratingStars.map((title, index) => (
-        <>
-          <input
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value={index}
-            id={`${index}-stars`}
-            type="radio"
-          />
-          <label
-            htmlFor={`${index}-stars`}
-            className="reviews__rating-label form__rating-label"
-            title={title}
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-        </>
-      ))}
+      {ratingStars.map((title, index, array) => {
+        const value = array.length - index;
+
+        return (
+          <>
+            <input
+              className="form__rating-input visually-hidden"
+              name="rating"
+              value={value}
+              id={`${value}-stars`}
+              type="radio"
+            />
+            <label
+              htmlFor={`${value}-stars`}
+              className="reviews__rating-label form__rating-label"
+              title={title}
+            >
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star"></use>
+              </svg>
+            </label>
+          </>
+        );
+      })}
     </div>
   );
 }
