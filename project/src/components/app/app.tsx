4e01e16a24ff98c1da175fc7e80b0dd05084623c
Christik <christik.lucid@gmail.com>
2022-11-12 10:06:24 +0300

    Добавляет кастомные хуки useAppDispatch, useAppSelector

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { AppRoute } from '../../const';
+
 import { Offer } from '../../types/offer';
 import { AllReviews } from '../../types/review';
+
+import { store } from '../../store';
 
 import MainScreen from '../../pages/main-screen/main-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
@@ -17,39 +21,41 @@ type AppProps = {
 
 function App(props: AppProps) {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Root}
-          element={
-            <MainScreen
-              roomCardCount={props.roomCardCount}
-              offers={props.offers}
-            />
-          }
-        />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={
+              <MainScreen
+                roomCardCount={props.roomCardCount}
+                offers={props.offers}
+              />
+            }
+          />
 
-        <Route
-          path={AppRoute.OfferItem}
-          element={
-            <RoomScreen
-              offers={props.offers}
-              allReviews={props.allReviews}
-            />
-          }
-        />
+          <Route
+            path={AppRoute.OfferItem}
+            element={
+              <RoomScreen
+                offers={props.offers}
+                allReviews={props.allReviews}
+              />
+            }
+          />
 
-        <Route
-          path={AppRoute.Login}
-          element={<LoginScreen />}
-        />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginScreen />}
+          />
 
-        <Route
-          path={AppRoute.NotFound}
-          element={<NotFoundScreen />}
-        />
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path={AppRoute.NotFound}
+            element={<NotFoundScreen />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ project/src/hooks/index.ts
@@ -0,0 +1,6 @@
+import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
+import { AppDispatch, State } from '../types/state';
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
