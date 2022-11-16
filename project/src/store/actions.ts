17a6862ab11d12f80cfd35e9d8226faf6d00d695
Christina Yanovich <christik.lucid@gmail.com>
2022-11-16 11:09:50 +0300

    Рефакторит actions.ts

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const changeCity = createAction('offers/changeCity', (city: string) => ({
-  payload: city,
+export const changeCity = createAction('offers/changeCity', (cityName: string) => ({
+  payload: cityName,
 }));
 
 export const updateOffers = createAction('offers/update', (cityName: string) => ({
