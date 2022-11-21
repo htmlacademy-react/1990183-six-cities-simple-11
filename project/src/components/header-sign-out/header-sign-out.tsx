d8b1456ca3dd54d77b31fe49f5f3c885692f16b4
Christik <christik.lucid@gmail.com>
2022-11-21 12:33:07 +0300

    Рефакторит HeaderSignOut

--- project/src/components/header-sign-out/header-sign-out.tsx
+++ project/src/components/header-sign-out/header-sign-out.tsx
@@ -1,18 +1,22 @@
+import { MouseEvent } from 'react';
+
 import { useAppDispatch } from '../../hooks';
 import { logoutAction } from '../../store/user/api-actions';
 
 function HeaderSignOut() {
   const dispatch = useAppDispatch();
 
+  const handleSignOutClick = (evt: MouseEvent<HTMLAnchorElement>) => {
+    evt.preventDefault();
+    dispatch(logoutAction());
+  };
+
   return (
     <li className="header__nav-item">
       <a
         className="header__nav-link"
         href="#signout"
-        onClick={(evt) => {
-          evt.preventDefault();
-          dispatch(logoutAction());
-        }}
+        onClick={handleSignOutClick}
       >
         <span className="header__signout">Sign out</span>
       </a>
