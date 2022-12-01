a1e9ebb8e8873cccd3442741b589d6bdfafcfcc5
Christik <christik.lucid@gmail.com>
2022-12-01 11:37:02 +0300

    Удаляет лишние функции из utils.ts

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,41 +1,5 @@
-enum LayoutSelector {
-  Page = '.page',
-  Main = '.page__main',
-}
-
 export const formatDate = (isoDate: string) => {
   const date = new Date(isoDate);
 
   return date.toLocaleString('en-US', { month: 'long', year: 'numeric' });
 };
-
-const toggleCssClassTo = (classNames: string[] | string, selector: string, removeFlag = false) => {
-  const element = document.querySelector(selector);
-
-  if (element === null) {
-    return;
-  }
-
-  if (typeof classNames === 'string') {
-    classNames = [classNames];
-  }
-
-  if (removeFlag) {
-    element.classList.remove(...classNames);
-    return;
-  }
-
-  element.classList.add(...classNames);
-};
-
-const createLayoutCss = (selector: LayoutSelector) => ({
-  add(classNames: string[] | string) {
-    toggleCssClassTo(classNames, selector);
-  },
-  remove(classNames: string[] | string) {
-    toggleCssClassTo(classNames, selector, true);
-  },
-});
-
-export const pageLayoutCss = createLayoutCss(LayoutSelector.Page);
-export const mainLayoutCss = createLayoutCss(LayoutSelector.Main);
