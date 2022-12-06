9bbc4e03ef48de410be723844dfca57a10bb58af
Christik <christik.lucid@gmail.com>
2022-12-06 14:38:17 +0300

    Обновляет вывод ошибок

--- project/src/store/offer/api-actions.ts
+++ project/src/store/offer/api-actions.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AxiosError, AxiosInstance } from 'axios';
+import { AxiosInstance } from 'axios';
 
 import { ApiRoute, AppRoute } from '../../const';
 
@@ -19,7 +19,6 @@ import {
   setReviewSentSuccessfullyStatus,
   setReviewSendingStatus,
   setReviewsLoadingStatus } from '../offer/actions';
-import { toast } from 'react-toastify';
 
 type ReviewData = {
   comment: string;
@@ -104,12 +103,6 @@ export const sendReviewAction = createAsyncThunk<void, ReviewRequestData, {
     }
 
     catch (error) {
-      const axiosError = error as AxiosError<{error: string}>;
-
-      if (axiosError.response) {
-        toast.error(axiosError.response.data.error);
-      }
-
       dispatch(setReviewSentSuccessfullyStatus(false));
     }
 
